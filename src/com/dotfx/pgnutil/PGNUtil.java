2cb6f3fd804e0d64c47e9ac90653f2734f6a8ca4
Mark Chen <chen@localhost.localdomain>
2025-02-04 12:52:10 -0800

    Updated version.

--- src/com/dotfx/pgnutil/PGNUtil.java
+++ src/com/dotfx/pgnutil/PGNUtil.java
@@ -793,7 +793,7 @@ public class PGNUtil
         }
     }
     
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.2";
 
     private static PgnGame game;
     private static final List<PgnFile> pgnFileList = new ArrayList<>();
