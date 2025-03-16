1334ec2d6e1304470e8f040325b23d27f02af29a
Mark Chen <chen@localhost.localdomain>
2025-03-16 02:08:47 -0700

    Code tweak.

--- src/com/dotfx/pgnutil/OpeningScore.java
+++ src/com/dotfx/pgnutil/OpeningScore.java
@@ -85,6 +85,6 @@ public class OpeningScore
             "white: " + Formats.PERCENT.format(whiteWinPct) + CLOptions.outputDelim +
             "black: " + Formats.PERCENT.format(blackWinPct) + CLOptions.outputDelim +
             "diff: " + Formats.PERCENT.format(whiteWinPct - blackWinPct) + CLOptions.outputDelim +
-            "draw: " + Formats.PERCENT.format(getDrawPct()) + CLOptions.outputDelim;
+            "draw: " + Formats.PERCENT.format(getDrawPct());
     }
 }
